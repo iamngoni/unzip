0ae22df5d6a32fa43ba1731f6e908f280d1d0aec
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-02-08 00:48:11 +0200

    feat: move files from folders and delete folder

--- unzip.py
+++ unzip.py
@@ -33,5 +33,26 @@ def unzip():
             logger.error(exc)
             logger.error('------------------Skipping----------------------')
 
+
+    # check if any folders were extracted and move files to the parent dir
+    folders = [f for f in os.listdir('.') if os.path.isdir(f)]
+    logger.info('Preparing to move files from {} folders.'.format(len(folders)))
+    logger.info('----------------------------------------')
+    for f in folders:
+        try:
+            logger.info('Moving files from folder: {}'.format(f))
+            files = [f for f in os.listdir(f) if os.path.isfile(f)]
+            for file in files:
+                file_abspath = os.path.abspath(file)
+                logger.info('Absolute path: {}'.format(file_abspath))
+                os.rename(file_abspath, os.path.join(os.path.abspath('.'), file))
+            logger.info('Deleting folder: {}'.format(f))
+            os.rmdir(f)
+            logger.info('----------------------------------------')
+        except Exception as exc:
+            logger.error('Error moving files from folder: {}'.format(f))
+            logger.error(exc)
+            logger.error('------------------Skipping----------------------')
+
 if __name__ == '__main__':
     unzip()
